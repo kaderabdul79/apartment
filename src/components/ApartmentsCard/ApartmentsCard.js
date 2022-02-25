285d647f809937d8dc3f02c302978590e4c972b0
kaderabdul <kaderabdul7980@gmail.com>
2022-02-25 17:43:41 +0600

    has been finish the signle room using grid

--- src/components/ApartmentsCard/ApartmentsCard.js
+++ src/components/ApartmentsCard/ApartmentsCard.js
@@ -1,15 +1,60 @@
 import React from 'react';
 import './ApartmentsCard.css';
 import room1 from './ApartmentImage/room1.jpg';
+import room3 from './ApartmentImage/room3.jpg';
+
 import topBanner from './ApartmentImage/breadcrumb.webp';
 
 const ApartmentsCard = () => {
     return (
         <div className='apartmentsCard'>
+            {/* top banner area */}
             <div className="banner-area">
                 <div className="banner-title"><h2>Apartments</h2></div>
                 <img src={topBanner} alt="" />
             </div>
+            {/* top banner end */}
+
+            {/* all rooms grid */}
+            <div className="apartments-area">
+                {/* single-apartment start */}
+                <div className="single-apartment">
+                    <img src={room1} alt="room" />
+                    <div className="apartment-desc">
+                        <h3>studio apartments</h3>
+                        <span>explore</span>
+                    </div>
+                </div>
+                {/* single-apartment end */}
+                {/* single-apartment start */}
+                <div className="single-apartment">
+                    <img src={room1} alt="room" />
+                    <div className="apartment-desc">
+                        <h3>studio apartments</h3>
+                        <span>explore</span>
+                    </div>
+                </div>
+                {/* single-apartment end */}
+                {/* single-apartment start */}
+                <div className="single-apartment">
+                    <img src={room3} alt="room" />
+                    <div className="apartment-desc">
+                        <h3>studio apartments</h3>
+                        <span>explore</span>
+                    </div>
+                </div>
+                {/* single-apartment end */}
+                                {/* single-apartment start */}
+                                <div className="single-apartment">
+                    <img src={room1} alt="room" />
+                    <div className="apartment-desc">
+                        <h3>studio apartments</h3>
+                        <span>explore</span>
+                    </div>
+                </div>
+                {/* single-apartment end */}
+            </div>
+            {/* all rooms grid end */}
         </div>
     );
 };
