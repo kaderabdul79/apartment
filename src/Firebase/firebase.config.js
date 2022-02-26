745351feab5eea3c3aba4e2466a2eb8d1ef1823e
kaderabdul <kaderabdul7980@gmail.com>
2022-02-26 10:02:02 +0600

    setup firebase config and initialize

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDNBfvli62rbOmK_KRiz0B8FBRY-EuR34M",
+    authDomain: "apartment-c3fc1.firebaseapp.com",
+    projectId: "apartment-c3fc1",
+    storageBucket: "apartment-c3fc1.appspot.com",
+    messagingSenderId: "223634394127",
+    appId: "1:223634394127:web:1f4b68748de80bcae8677a"
+  };
+
+  export default firebaseConfig;
