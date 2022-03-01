c00646396e369d8a395a834855113b033373fa23
kaderabdul <kaderabdul7980@gmail.com>
2022-03-01 18:11:27 +0600

    collect contact data and send to server also show success message

--- src/components/CustomForm/CustomForm.js
+++ src/components/CustomForm/CustomForm.js
@@ -1,23 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CustomForm.css';
 import InputBox from '../InputBox/InputBox';
 
 const CustomForm = () => {
+    const [contactInfo,setContactInfo] = useState({})
+    const [successmsg,setSuccessmsg] = useState(false)
+
+    const handleOnBlur = (e) => {
+      const field = e.target.name;
+      const value = e.target.value;
+      const newcontactInfo = { ...contactInfo };
+      newcontactInfo[field] = value;
+      setContactInfo(newcontactInfo);
+    }
+    
+    const handleContactForm = (e) => {
+        // send info to server
+        fetch('http://localhost:5000/contacts', {
+            method: 'POST',
+            headers: {
+              'content-type': 'application/json'
+            },
+            body: JSON.stringify(contactInfo)
+          })
+          .then(res => res.json())
+          .then(data => {
+            // console.log(data)
+            if(data.insertedId){
+                setContactInfo('')
+                setSuccessmsg(true)
+                // 
+            }
+          })
+        // prevent form default behaviour
+        e.preventDefault();
+    }
     return (
         <div className='form-area'>
             <div className="form-title">GET IN TOUCH !</div>
             <div className="form-cover">
                 <div className="left-side">
-                    <form>
-                        <InputBox type="text" name="name" placeholder="Your Name"></InputBox>
-                        <InputBox type="email" name="email" placeholder="Your email"></InputBox>
-                        <InputBox type="textarea" name="message" placeholder="Your Message"></InputBox>
+                    <form onSubmit={handleContactForm}>
+                        <InputBox eventListener={handleOnBlur} type="text" name="name" placeholder="Your Name"></InputBox>
+                        <InputBox eventListener={handleOnBlur} type="email" name="email" placeholder="Your email"></InputBox>
+                        <InputBox eventListener={handleOnBlur} type="textarea" name="message" placeholder="Your Message"></InputBox>
                         <input type="submit" value="Send Message" />
+                        { successmsg && <div>Successfully Message Sent</div> }
                     </form>
                 </div>
-                {/* <div className="right-side">
-                    hello
-                </div> */}
             </div> 
         </div>
     );
