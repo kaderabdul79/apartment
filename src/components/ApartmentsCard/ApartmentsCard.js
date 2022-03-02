e6bc912094ec90ad75ab34a07b2c16076c35dd56
kaderabdul <kaderabdul7980@gmail.com>
2022-03-02 20:19:24 +0600

    all the data fetch from server using api

--- src/components/ApartmentsCard/ApartmentsCard.js
+++ src/components/ApartmentsCard/ApartmentsCard.js
@@ -5,48 +5,19 @@ import room3 from './ApartmentImage/room3.jpg';
 import TopBanner from '../TopBanner/TopBanner';
 import { Link } from 'react-router-dom';
 
-const ApartmentsCard = () => {
+const ApartmentsCard = (props) => {
+    // console.log(props.apartment)
+    const {title,image} = props.apartment
     return (
-        <div className='apartmentsCard'>
-            {/* top banner area */}
-            <TopBanner title="Apartments"></TopBanner>
-            {/* top banner end */}
-
-            {/* all rooms grid */}
-            <div className="apartments-area">
-                {/* single-apartment start */}
-                <div className="single-apartment">
-                    <img src={room1} alt="room" />
+        <div>
+            <div className="single-apartment">
+                    <img src={`data:image/png;base64,${image}`} alt="room" />
+                    {/* <img src={room1} alt="room" /> */}
                     <Link to="/"><div className="apartment-desc">
-                        <h3>studio apartments</h3>
+                        <h3>{title}</h3>
                         <span>explore</span>
                     </div></Link>
                 </div>
-                <div className="single-apartment">
-                    <img src={room1} alt="room" />
-                    <Link to="/"><div className="apartment-desc">
-                        <h3>studio apartments</h3>
-                        <span>explore</span>
-                    </div></Link>
-                </div>
-                <div className="single-apartment">
-                    <img src={room1} alt="room" />
-                    <Link to="/"><div className="apartment-desc">
-                        <h3>studio apartments</h3>
-                        <span>explore</span>
-                    </div></Link>
-                </div>
-                <div className="single-apartment">
-                    <img src={room1} alt="room" />
-                    <Link to="/"><div className="apartment-desc">
-                        <h3>studio apartments</h3>
-                        <span>explore</span>
-                    </div></Link>
-                </div>
-                {/* single-apartment end */}
-                
-
-            </div>
             {/* all rooms grid end */}
         </div>
     );
--- src/pages/Apartments.js
+++ src/pages/Apartments.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import '../components/ApartmentsCard/ApartmentsCard.css';
 import ApartmentsCard from '../components/ApartmentsCard/ApartmentsCard';
+import TopBanner from '../components/TopBanner/TopBanner';
 
 const Apartments = () => {
+    const [apartments,setApartments] = useState([])
+    // load data from server
+    useEffect(()=>{
+        fetch('http://localhost:5000/apartments')
+        .then(res => res.json())
+        .then(data => setApartments(data))
+    },[])
+
     return (
-        <div>
-            <ApartmentsCard></ApartmentsCard>
+        <div className='apartmentsCard'>
+            <TopBanner title="Apartments"></TopBanner>
+            {/* {apartments.length} */}
+            <div className="apartments-area">
+            {/* <div className="single-apartment"> */}
+            {
+                apartments.map(apartment => <ApartmentsCard apartment={apartment}></ApartmentsCard>)
+            }
+            {/* </div> */}
+            </div>
+            {/* <ApartmentsCard></ApartmentsCard> */}
         </div>
     );
 };
