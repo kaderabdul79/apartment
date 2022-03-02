710bea5bc62e910859229773871c0352a3e19498
kaderabdul <kaderabdul7980@gmail.com>
2022-03-02 18:14:02 +0600

    collect formdata and image and send to server

--- src/pages/Dashboard/AddApartment/AddApartment.js
+++ src/pages/Dashboard/AddApartment/AddApartment.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import InputBox from '../../../components/InputBox/InputBox';
 
 
 const AddApartment = () => {
-
-    const handleOnBlur = (e) => {
-        const field = e.target.name;
-        const value = e.target.value;
-      }
+    // const [apartmentInfo,setApartmentInfo] = useState({})
+    const [title,setTitle] = useState('')
+    const [description,setDescription] = useState('')
+    const [subtextarea,setTsubtextarea] = useState('')
+    const [image,setImage] = useState(null)
 
     const handleApartmentForm = (e) => {
-       e.preventDefault();
+        e.preventDefault();
+        // 
+        const formData = new FormData();
+        formData.append('title', title);
+        formData.append('description', description);
+        formData.append('subtextarea', subtextarea);
+        formData.append('image', image);
+        // send to server 
+        fetch('http://localhost:5000/apartments', {
+            method: 'POST',
+            body: formData
+        })
+    }
+    // file field style
+    const filestyle = {
+        width: '350px',
+        height: '75px',
+        padding: '20px',
+        border: '1px solid #CABE9F',
+        marginBottom: '20px',
+        borderRadius: '4px',
+        fontSize: '18px'
     }
     return (
         <div>
@@ -20,10 +41,10 @@ const AddApartment = () => {
                     <Col lg="4">
                         <h3 style={{textTransform: 'capitalize'}}>add new apartment</h3>
                         <form className="login-form" onSubmit={handleApartmentForm}>
-                        <InputBox eventListener={handleOnBlur} type="text" name="title" placeholder="Enter title"></InputBox>
-                        <InputBox eventListener={handleOnBlur} type="textarea" name="description" placeholder="Write description"></InputBox>
-                        <InputBox eventListener={handleOnBlur} type="file" name="featureImg" placeholder="Upload featureImg"></InputBox>
-                        <InputBox eventListener={handleOnBlur} type="text" name="title" placeholder="Enter title"></InputBox>
+                        <input className='allfield' onChange = { e => setTitle(e.target.value)} type="text" name="title" placeholder="Enter title"/>
+                        <input className='allfield' onChange = { e => setDescription(e.target.value)} type="textarea" name="description" placeholder="Write description"/>
+                        <input style={filestyle} onChange = { e => setImage(e.target.files[0])} type="file" accept="image/*" name="featureImg" placeholder="Upload featureImg" />
+                        <input className='allfield' onChange = { e => setTsubtextarea(e.target.value)} type="textarea" name="subtextarea" placeholder="Enter sub details"/>
 
                         <input type="submit" value="Add Apartment" />
                         </form>
